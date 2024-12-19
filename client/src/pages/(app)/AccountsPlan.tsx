7d5001db4487dd63db008fc86cb1e02fa7b0ffc5
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-12-19 14:16:34 -0400

    chore: initiate AccountsPlan.tsx file to establish foundational structure for account plan features in the application

--- client/src/pages/(app)/AccountsPlan.tsx
+++ client/src/pages/(app)/AccountsPlan.tsx
@@ -1,7 +1,7 @@
 export default function AccountsPlan() {
 	return (
 		<div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
-			<h1>Accounts Plan</h1>
+			<h1>Plan de cuentas</h1>
 		</div>
 	);
 }
