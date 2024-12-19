ca72898a2d7c6659feb5e7b7b393a9b4e9519d63
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-12-18 21:17:20 -0400

    feat: add missing validation for account plan inputs to improve integrity and error handling

---
+++ client/src/services/account-plan.ts
@@ -0,0 +1,47 @@
+import { AccountPlanSchema, type IAccountPlanForCreate } from '@contapp/shared';
+
+export default class AccountPlanService {
+	static async findAll(companyId: string) {
+		try {
+			const request = await fetch(`/accounts-plan/findAll/${companyId}`);
+
+			const response = await request.json();
+
+			return AccountPlanSchema.array().parse(response?.data?.accounts_plan);
+		} catch (error) {
+			console.error('AccountPlanService.findAll', error);
+			throw error;
+		}
+	}
+
+	static async findById(accountId: string, planId: string) {
+		try {
+			const request = await fetch(
+				`/accounts-plan/findById/${accountId}/${planId}`,
+			);
+
+			const response = await request.json();
+
+			return AccountPlanSchema.parse(response?.data?.accounts_plan);
+		} catch (error) {
+			console.error('AccountPlanService.findById', error);
+			throw error;
+		}
+	}
+
+	static async create(companyId: string, plan: IAccountPlanForCreate) {
+		try {
+			const request = await fetch('/accounts-plan/create', {
+				method: 'POST',
+				body: JSON.stringify(plan),
+			});
+
+			const response = await request.json();
+
+			return AccountPlanSchema.parse(response?.data?.accounts_plan);
+		} catch (error) {
+			console.error('AccountPlanService.create', error);
+			throw error;
+		}
+	}
+}
