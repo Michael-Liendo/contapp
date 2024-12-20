4b9f77842e0753efdc05638737518a27163a7c8c
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-12-20 17:07:38 -0400

    chore: integrate SidebarInset into AppLayout for enhanced sidebar management and layout structure improvements

--- client/src/components/app-layout.tsx
+++ client/src/components/app-layout.tsx
@@ -1,22 +1,28 @@
 import { AppSidebar } from '@/components/nav-bar/app-sidebar';
 import { Separator } from '@/components/ui/separator';
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from '@/components/ui/sidebar';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<SidebarProvider>
 			<AppSidebar />
-			<div className='flex flex-col h-screen'>
-				<header className='flex h-16 items-center justify-between px-4'>
-					<div className='flex items-center gap-2 px-4'>
-						<SidebarTrigger className='-ml-1' />
-						<Separator orientation='vertical' className='mr-2 h-4' />
-					</div>
-				</header>
-				<main className='flex flex-col flex-1 overflow-auto gap-4 p-4 pt-0'>
-					{children}
-				</main>
-			</div>
+			<SidebarInset>
+				<div className='flex flex-col h-screen'>
+					<header className='flex h-16 items-center justify-between px-4'>
+						<div className='flex items-center gap-2 px-4'>
+							<SidebarTrigger className='-ml-1' />
+							<Separator orientation='vertical' className='mr-2 h-4' />
+						</div>
+					</header>
+					<main className='flex flex-col flex-1 overflow-auto gap-2 px-8 pt-0'>
+						{children}
+					</main>
+				</div>
+			</SidebarInset>
 		</SidebarProvider>
 	);
 }
