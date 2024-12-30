c5ee3077400d05161157459062b1d39772dbfe90
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-12-29 21:58:32 -0400

    feat: add journal schema and interface definitions for better journal management and entry validation using zod

--- shared/src/schema/index.ts
+++ shared/src/schema/index.ts
@@ -1,3 +1,4 @@
 export * from './user';
 export * from './company';
 export * from './account_plan';
+export * from './journal';
---
+++ shared/src/schema/journal.ts
@@ -0,0 +1,61 @@
+import { z } from 'zod';
+
+const journalDestination = z.enum(['DEBIT', 'CREDIT']);
+
+export const JournalSchema = z.object({
+	id: z.string().describe('The unique identifier of the journal'),
+	company_id: z
+		.string()
+		.describe(
+			'The unique identifier of the company associated with the journal',
+		),
+	description: z
+		.string()
+		.optional()
+		.describe('A brief description of the journal'),
+	destination: journalDestination.describe('The destination of the journal'),
+	entry_date: z.coerce.date().describe('The date of the journal'),
+	created_at: z.coerce
+		.date()
+		.describe('Timestamp when the journal was created'),
+	updated_at: z.coerce
+		.date()
+		.describe('Timestamp when the journal was last updated'),
+});
+
+export const JournalEntrySchema = z.object({
+	id: z.string().describe('The unique identifier of the journal entry'),
+	journal_id: z.string().describe('The unique identifier of the journal'),
+	account_id: z.string().describe('The unique identifier of the account'),
+	description: z
+		.string()
+		.optional()
+		.describe('The description of the journal entry'),
+	debit: z
+		.number()
+		.min(0)
+		.default(0)
+		.describe('The debit amount for this journal entry'),
+	credit: z
+		.number()
+		.min(0)
+		.default(0)
+		.describe('The credit amount for this journal entry'),
+	created_at: z.coerce.date(),
+	updated_at: z.coerce.date(),
+});
+
+export const JournalEntryForCreateSchema = JournalEntrySchema.omit({
+	id: true,
+	created_at: true,
+	updated_at: true,
+});
+
+export const JournalForCreateSchema = JournalSchema.omit({
+	id: true,
+	created_at: true,
+	updated_at: true,
+}).extend({
+	destination: journalDestination,
+	journal_entries: z.array(JournalEntryForCreateSchema),
+});
