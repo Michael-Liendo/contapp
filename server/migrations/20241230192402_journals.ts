dc2b5ac19f71c664962655af64fa1cd54bab3c16
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-12-31 13:30:29 -0400

    feat: add journal_number field and triggers for automatic number generation in journals schema and migration

--- server/migrations/20241230192402_journals.ts
+++ server/migrations/20241230192402_journals.ts
@@ -8,6 +8,7 @@ export async function up(knex: Knex): Promise<void> {
 			.unique()
 			.defaultTo(knex.raw('uuid_generate_v4()'))
 			.primary();
+		table.integer('journal_number').notNullable();
 		table
 			.uuid('company_id')
 			.notNullable()
@@ -18,8 +19,36 @@ export async function up(knex: Knex): Promise<void> {
 		table.date('entry_date').notNullable();
 		table.timestamps(true, true);
 	});
+
+	await knex.raw(`
+    CREATE OR REPLACE FUNCTION generate_journal_number()
+    RETURNS TRIGGER AS $$
+    DECLARE
+      max_number INTEGER;
+    BEGIN
+      SELECT COALESCE(MAX(journal_number), 0) + 1 INTO max_number
+      FROM journals
+      WHERE company_id = NEW.company_id;
+      
+      NEW.journal_number = max_number;
+      RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;
+  `);
+
+	// Crear un trigger que use la función
+	await knex.raw(`
+    CREATE TRIGGER set_journal_number
+    BEFORE INSERT ON journals
+    FOR EACH ROW
+    EXECUTE FUNCTION generate_journal_number();
+  `);
 }
 
 export async function down(knex: Knex): Promise<void> {
+	await knex.raw('DROP TRIGGER IF EXISTS set_journal_number ON journals');
+
+	await knex.raw('DROP FUNCTION IF EXISTS generate_journal_number');
+
 	await knex.schema.dropTableIfExists('journals');
 }
