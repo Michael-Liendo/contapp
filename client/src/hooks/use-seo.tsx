111b1e1366c00dd415b005a6db959b517daf798b
gusvet18 <gus1465@yahoo.com>
2025-01-08 12:07:54 -0500

    update: run npm format

--- client/src/hooks/use-seo.tsx
+++ client/src/hooks/use-seo.tsx
@@ -1,35 +1,37 @@
 import { useEffect } from 'react';
 
 interface SEOProps {
-    title: string;
-    description: string;
-    keywords?: string;
-    author?: string;
-    robots?: string;
+	title: string;
+	description: string;
+	keywords?: string;
+	author?: string;
+	robots?: string;
 }
 
 const useSEO = ({ title, description, keywords, author, robots }: SEOProps) => {
-    useEffect(() => {
-        document.title = title;
+	useEffect(() => {
+		document.title = title;
 
-        const setMetaTag = (name: string, content: string | undefined) => {
-            if (!content) return;
-            let element = document.querySelector(`meta[name="${name}"]`) as HTMLMetaElement | null;
-            if (element) {
-                element.setAttribute('content', content);
-            } else {
-                element = document.createElement('meta') as HTMLMetaElement;
-                element.name = name;
-                element.content = content;
-                document.head.appendChild(element);
-            }
-        };
+		const setMetaTag = (name: string, content: string | undefined) => {
+			if (!content) return;
+			let element = document.querySelector(
+				`meta[name="${name}"]`,
+			) as HTMLMetaElement | null;
+			if (element) {
+				element.setAttribute('content', content);
+			} else {
+				element = document.createElement('meta') as HTMLMetaElement;
+				element.name = name;
+				element.content = content;
+				document.head.appendChild(element);
+			}
+		};
 
-        setMetaTag('description', description);
-        setMetaTag('keywords', keywords);
-        setMetaTag('author', author);
-        setMetaTag('robots', robots);
-    }, [title, description, keywords, author, robots]);
+		setMetaTag('description', description);
+		setMetaTag('keywords', keywords);
+		setMetaTag('author', author);
+		setMetaTag('robots', robots);
+	}, [title, description, keywords, author, robots]);
 };
 
 export default useSEO;
--- client/src/pages/(app)/Home.tsx
+++ client/src/pages/(app)/Home.tsx
@@ -1,13 +1,13 @@
-import useSEO from "@/hooks/use-seo";
+import useSEO from '@/hooks/use-seo';
 
 export default function HomeApp() {
-
 	useSEO({
 		title: 'Home Page - Contapp',
-		description: 'Welcome to the home page of Contapp, your app for managing contacts.',
+		description:
+			'Welcome to the home page of Contapp, your app for managing contacts.',
 		keywords: 'contacts, management, app, Contapp',
 		author: 'Contapp Team',
-		robots: 'index, follow'
+		robots: 'index, follow',
 	});
 
 	return (
