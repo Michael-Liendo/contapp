6aebad8dd71acb03d754c97bdb429546431fd866
gusvet18 <gus1465@yahoo.com>
2025-01-08 07:47:11 -0500

    feat: hook for use SEO

--- client/src/pages/(app)/Home.tsx
+++ client/src/pages/(app)/Home.tsx
@@ -1,4 +1,15 @@
+import useSEO from "@/hooks/use-seo";
+
 export default function HomeApp() {
+
+	useSEO({
+		title: 'Home Page - Contapp',
+		description: 'Welcome to the home page of Contapp, your app for managing contacts.',
+		keywords: 'contacts, management, app, Contapp',
+		author: 'Contapp Team',
+		robots: 'index, follow'
+	});
+
 	return (
 		<div>
 			<h1>Hello world</h1>
