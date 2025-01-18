bfdab716a2f9d27edbd95e4bc4e0b0624c4dea53
daridjcm <daricorreamedina@gmail.com>
2025-01-18 12:54:57 -0500

    Enhace UI Responsive and code organization

--- client/src/pages/(app)/Profile.tsx
+++ client/src/pages/(app)/Profile.tsx
@@ -2,11 +2,11 @@ import { TextField } from '@/components/text-field';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import {
-	Card,
-	CardContent,
-	CardFooter,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from '@/components/ui/card';
 import { toast } from '@/components/ui/use-toast';
 import useAuth from '@/hooks/useAuth';
@@ -18,114 +18,114 @@ import { Check, PencilLine } from 'lucide-react';
 import { useState } from 'react';
 
 export default function Profile() {
-	const { user } = useAuth();
+  const { user } = useAuth();
 
-	// cambiar nombre a uno mejor, mas semantico
-	const [inputView, setInputView] = useState({
-		email: false,
-		first_name: false,
-		last_name: false,
-	});
+  const [fieldEditMode, setFieldEditMode] = useState({
+    email: false,
+    first_name: false,
+    last_name: false,
+  });
 
-	const { values, handleChange, handleSubmit } = useFormik({
-		initialValues: {
-			first_name: user?.first_name,
-			last_name: user?.last_name,
-			email: user?.email,
-			password: undefined,
-			old_password: undefined,
-		},
-		validationSchema: toFormikValidationSchema(UserForUpdateSchema),
-		onSubmit: async (values) => {
-			const updated = await Services.users.update(values);
+  const toggleEditField = (field) => {
+    setFieldEditMode((prev) => ({
+      ...prev,
+      [field]: !prev[field],
+    }));
+  };
 
-			toast({
-				title: 'Tu usuario fue actualizado',
-			});
+  const { values, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      first_name: user?.first_name,
+      last_name: user?.last_name,
+      email: user?.email,
+      password: undefined,
+      old_password: undefined,
+    },
+    validationSchema: toFormikValidationSchema(UserForUpdateSchema),
+    onSubmit: async (values) => {
+      const updated = await Services.users.update(values);
 
-			console.log(updated);
-		},
-	});
+      toast({
+        title: 'Tu usuario fue actualizado',
+      });
 
-	return (
-		<Card>
-			<CardHeader>
-				<div className='flex items-center gap-2 px-1 py-1.5 text-left text-sm mb-2'>
-					<Avatar className='h-28 w-28 rounded-lg'>
-						<AvatarImage alt={user?.first_name} />
-						<AvatarFallback className='rounded-l'>
-							{user?.first_name.at(0)}
-							{user?.last_name.at(0)}
-						</AvatarFallback>
-					</Avatar>
-				</div>
-				<CardTitle>Hola, {user?.first_name}</CardTitle>
-				<p className='opacity-50'>Edita tu información de perfil</p>
-			</CardHeader>
-			<form id='create-company' noValidate onSubmit={handleSubmit}>
-				<CardContent>
-					<div className='flex flex-wrap gap-5'>
-						<div className='flex gap-1 w-full sm:w-full lg:w-[35%] space-y-6'>
-							<TextField
-								label='Correo'
-								type='text'
-								id='user_email'
-								placeholder={user?.email}
-								// todo: y esto a los otros input
-								readOnly={!inputView.email}
-							/>
-							<Button
-								variant='outline'
-								size='icon'
-								type='button'
-								onClick={() => {
-									//hacer esto con los demas, first_name last_name
-									setInputView({ ...inputView, email: true });
+      console.log(updated);
+    },
+  });
 
-									// esto se puede hacer luego de que cuando le de click te lleve directamente el input
-								}}
-							>
-								<PencilLine />
-							</Button>
-						</div>
-						<div className='flex gap-1 w-full sm:w-full lg:w-[30%] space-y-6'>
-							<TextField
-								label='Nombres'
-								type='text'
-								id='user_first_name'
-								value={values.first_name}
-								placeholder={user?.first_name}
-								readOnly
-							/>
-							<Button variant='outline' size='icon' type='button'>
-								<PencilLine />
-							</Button>
-						</div>
-						<div className='flex gap-1 w-full sm:w-full lg:w-[30%] space-y-6'>
-							<TextField
-								label='Apellidos'
-								type='text'
-								id='user_last_name'
-								value={values.last_name}
-								placeholder={user?.last_name}
-								onChange={handleChange}
-								readOnly
-							/>
-							<Button variant='outline' size='icon' type='button'>
-								<PencilLine />
-							</Button>
-						</div>
-					</div>
-				</CardContent>
-				<CardFooter>
-					<div className='flex w-full lg:justify-end sm:justify-center'>
-						<Button variant='default' size='sm' type='submit'>
-							<Check className='opacity-50' />
-							Confirmar cambios
-						</Button>
-					</div>
-				</CardFooter>
-			</form>
-		</Card>
-	);
+  return (
+    <Card>
+      <CardHeader>
+        <div className='flex items-center gap-2 px-1 py-1.5 text-left text-sm mb-2'>
+          <Avatar className='h-28 w-28 rounded-full'>
+            <AvatarImage alt={user?.first_name} />
+            <AvatarFallback className='rounded-full'>
+              {user?.first_name.at(0)}
+              {user?.last_name.at(0)}
+            </AvatarFallback>
+          </Avatar>
+        </div>
+        <CardTitle>Hola, {user?.first_name}</CardTitle>
+        <p className='opacity-50'>Edita tu información de perfil</p>
+      </CardHeader>
+      <form noValidate onSubmit={handleSubmit}>
+        <CardContent>
+          <div className='flex flex-wrap gap-5'> 
+            {[
+              // i make this array to avoid repeat the same code
+              {
+                label: 'Correo',
+                id: 'user_email',
+                value: values.email,
+                placeholder: user?.email,
+                isEditable: fieldEditMode.email,
+                onEdit: () => toggleEditField('email'),
+              },
+              {
+                label: 'Nombres',
+                id: 'user_first_name',
+                value: values.first_name,
+                placeholder: user?.first_name,
+                isEditable: fieldEditMode.first_name,
+                onEdit: () => toggleEditField('first_name'),
+              },
+              {
+                label: 'Apellidos',
+                id: 'user_last_name',
+                value: values.last_name,
+                placeholder: user?.last_name,
+                isEditable: fieldEditMode.last_name,
+                onEdit: () => toggleEditField('last_name'),
+              },
+            ].map(({ label, id, value, placeholder, isEditable, onEdit }) => (
+              <div key={id} className='flex gap-1 w-full sm:w-full lg:w-[30%] space-y-6'>
+                <TextField
+                  label={label}
+                  type='text'
+                  id={id}
+                  value={isEditable ? value : ''} 
+                  // placeholder TODO: change this placeholder to value, so yet not working good
+                  placeholder={!isEditable ? placeholder : ''}  
+                  readOnly={!isEditable}
+                  onChange={handleChange}
+                />
+                <Button variant='outline' size='icon' type='button' onClick={onEdit}>
+                  <PencilLine />
+                </Button>
+              </div>
+            ))}
+          </div>
+        </CardContent>
+
+        <CardFooter>
+          <div className='flex w-full lg:justify-end sm:justify-center'>
+            <Button variant='default' size='sm' type='submit'>
+              <Check className='opacity-50' />
+              Confirmar cambios
+            </Button>
+          </div>
+        </CardFooter>
+      </form>
+    </Card>
+  );
 }
