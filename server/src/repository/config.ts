fb788a7494a31e182540f5b853443a324ad97ba8
Michael Liendo <michael.m.liendo.r@gmail.com>
2025-01-22 13:53:58 -0400

    chore(config): add SSL configuration to knex for enhanced security in production environments

--- server/src/repository/config.ts
+++ server/src/repository/config.ts
@@ -18,6 +18,9 @@ const knexConfig: { [key: string]: Knex.Config } = {
 			user: process.env.POSTGRES_USER,
 			password: process.env.POSTGRES_PASSWORD,
 			database: process.env.POSTGRES_DB,
+			ssl: {
+				rejectUnauthorized: false,
+			},
 		},
 		pool: { min: 0, max: 10 },
 	},
