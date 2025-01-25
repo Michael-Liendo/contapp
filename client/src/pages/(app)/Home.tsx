e4fff7acd7f9e4c05ea380836971acf438452247
Dariana <daricorreamedina@gmail.com>
2025-01-24 19:44:54 -0500

    View Home (#55)
    * Fix bug to Register
    
    * Add view home
    
    * Add routes to ButtonTooltip link
    
    * Changes path to private routes
    
    * Run format

--- client/src/pages/(app)/Home.tsx
+++ client/src/pages/(app)/Home.tsx
@@ -1,3 +1,11 @@
+import ButtonTooltip from '@/components/home/ButtonTooltip';
+import { textGettingStart } from '@/components/home/content';
+import {
+	Card,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
 import useSEO from '@/hooks/use-seo';
 
 export default function HomeApp() {
@@ -11,8 +19,30 @@ export default function HomeApp() {
 	});
 
 	return (
-		<div>
-			<h1>Hello world</h1>
+		<div className='container mx-auto px-4 py-8'>
+			<header className='text-center mb-12'>
+				<h1 className='text-4xl font-bold mb-4'>Bienvenido a Contapp</h1>
+				<p className='text-xl text-muted-foreground'>
+					La solución para gestionar operaciones contables, balances y plan de
+					cuentas.
+				</p>
+			</header>
+			<section>
+				<h2 className='text-2xl font-semibold mb-4'>Primeros pasos</h2>
+				<div className='grid md:grid-cols-1 lg:grid-cols-3 gap-6'>
+					{textGettingStart.map((step) => (
+						<Card key={step.id}>
+							<CardHeader>
+								<div className='flex mb-3'>
+									<CardTitle className='mr-2'>{step.title}</CardTitle>
+									<ButtonTooltip link={step.link} />
+								</div>
+								<CardDescription>{step.description}</CardDescription>
+							</CardHeader>
+						</Card>
+					))}
+				</div>
+			</section>
 		</div>
 	);
 }
