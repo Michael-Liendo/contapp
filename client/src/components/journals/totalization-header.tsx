c2167911315d127add394f241615755c88c4c2be
Michael Liendo <michael.m.liendo.r@gmail.com>
2025-01-25 15:05:50 -0400

    feat: Update TotalizingHeader labels from Spanish "débito/crédito" to "deber/haber" for consistency in terminology

--- client/src/components/journals/totalization-header.tsx
+++ client/src/components/journals/totalization-header.tsx
@@ -23,13 +23,13 @@ export function TotalizingHeader({
 	return (
 		<div className='flex items-center mt-3 space-x-3'>
 			<div>
-				<span className='font-semibold mr-3'>Total débito:</span>
+				<span className='font-semibold mr-3'>Total deber:</span>
 				<span className='text-xl font-bold'>
 					{formatCurrencyValue(finalTotalDebit)}
 				</span>
 			</div>
 			<div>
-				<span className='font-semibold mr-3'>Total crédito:</span>
+				<span className='font-semibold mr-3'>Total haber:</span>
 				<span className='text-xl font-bold'>
 					{formatCurrencyValue(finalTotalCredit)}
 				</span>
