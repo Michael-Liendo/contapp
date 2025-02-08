f2a0901da06d5794a759953d84d5f3c4c4f3a809
Michael Liendo <michael.m.liendo.r@gmail.com>
2025-02-08 10:09:36 -0400

    fix: reorder imports in trial balance interface
    * Moved TrialBalanceRequestSchema and TrialBalanceSchema import to correct location

--- shared/src/interfaces/trial_balance.ts
+++ shared/src/interfaces/trial_balance.ts
@@ -1,5 +1,5 @@
-import type { TrialBalanceRequestSchema, TrialBalanceSchema } from '../schema';
 import type { z } from 'zod';
+import type { TrialBalanceRequestSchema, TrialBalanceSchema } from '../schema';
 
 export interface ITrialBalanceRequest
 	extends z.infer<typeof TrialBalanceRequestSchema> {}
