27156c25995b03f8e4bf718cce6f1f32d33f2e78
Michael Liendo <michael.m.liendo.r@gmail.com>
2025-04-13 15:47:12 -0400

    fix: handle absence of token in AuthContext to prevent unnecessary user query execution and improve load handling

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -23,6 +23,7 @@ export const AuthProvider = ({ children }: { children?: React.ReactNode }) => {
 	);
 
 	const { data: user, isLoading } = useQuery(['user'], async () => {
+		if (!token) return;
 		const user = await Services.users.me();
 		return user;
 	});
