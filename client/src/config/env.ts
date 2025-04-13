943a9def78cbf75b1d3f0f05ba17d8dead5ae3e0
Michael Liendo <michael.m.liendo.r@gmail.com>
2025-04-13 00:42:57 -0400

    chore: improvement in the frontend

---
+++ client/src/config/env.ts
@@ -0,0 +1,14 @@
+const { VITE_API_URL = 'http://127.0.0.1:3000', NODE_ENV = 'development' } =
+	import.meta.env.VITE_API_URL as { VITE_API_URL: string; NODE_ENV: string };
+
+export const EnvConfig = () => {
+	if (NODE_ENV === 'production') {
+		return {
+			apiUrl: VITE_API_URL,
+		};
+	}
+
+	return {
+		apiUrl: VITE_API_URL,
+	};
+};
