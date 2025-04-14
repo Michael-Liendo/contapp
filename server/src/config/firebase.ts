281139fa3888d5d1ca5c058e51f81f6fb8b5b239
Michael Liendo <michael.m.liendo.r@gmail.com>
2025-04-14 11:46:33 -0400

    feat: update Firebase credentials handling by importing JSON for improved security and integrate resolveJsonModule in tsconfig

--- server/src/config/firebase.ts
+++ server/src/config/firebase.ts
@@ -1,10 +1,9 @@
 import { credential } from 'firebase-admin';
 import * as firebaseAdmin from 'firebase-admin';
 import type { AppOptions } from 'firebase-admin/app';
+import json from '../../firebase-service-account.json';
 
-const credentials = credential.cert(
-	require('../../firebase-service-account.json'),
-);
+const credentials = credential.cert(json as firebaseAdmin.ServiceAccount);
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
