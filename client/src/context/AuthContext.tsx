2f639fca2959bebee107b630a93c56a02e4def28
Michael Liendo <michael.m.liendo.r@gmail.com>
2025-04-14 10:17:38 -0400

    feat: reorganize imports in AuthContext for improved clarity and maintainability in Firebase usage

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useState } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
+import { createContext, useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 import Services from '../services';
 
