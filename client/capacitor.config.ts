d9ceb4a5cebf4ae6bce5500e11736538c4501db5
Michael Liendo <michael.m.liendo.r@gmail.com>
2025-04-14 21:12:33 -0400

    feat: enable Push Notifications in Capacitor configuration for enhanced user engagement and notifications support

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -4,6 +4,11 @@ const config: CapacitorConfig = {
 	appId: 'com.contapp.app',
 	appName: 'Contapp',
 	webDir: 'dist',
+	plugins: {
+		PushNotifications: {
+			presentationOptions: ['badge', 'sound', 'alert'],
+		},
+	},
 };
 
 export default config;
