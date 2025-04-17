bb0e732ba181724415b53c2bf40a25fad3538743
Michael Liendo <michael.m.liendo.r@gmail.com>
2025-04-16 20:30:33 -0400

    feat: remove unused query from Profile component to streamline data handling and improve performance

--- client/src/pages/(app)/Profile.tsx
+++ client/src/pages/(app)/Profile.tsx
@@ -17,7 +17,7 @@ import { UserForUpdateSchema } from '@contapp/shared';
 import { useFormik } from 'formik';
 import { Check, PencilLine } from 'lucide-react';
 import { useEffect, useState } from 'react';
-import { useQuery, useQueryClient } from 'react-query';
+import { useQueryClient } from 'react-query';
 
 export default function Profile() {
 	useSEO({
@@ -87,17 +87,6 @@ export default function Profile() {
 			},
 		});
 	}, [user]);
-
-	const { data: products } = useQuery(
-		['stripe-list-products'],
-		async () => {
-			const data = await Services.stripe.listProducts();
-			return data;
-		},
-		{
-			enabled: !!user?.id,
-		},
-	);
 
 	return (
 		<Card>
